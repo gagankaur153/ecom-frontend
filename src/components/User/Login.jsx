2159225a387e488074ddc3c468d83f3c4c90a713
dev105 <swalekhan1522@gmail.com>
2025-11-26 17:52:17 +0530

    css change

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -18,22 +18,22 @@ const Login = () => {
   }
   return (
    <>
-    <div className='  h-[90vh] flex items-center mt-28 justify-center '>
-     <form onSubmit={handleform} className=' container shadow-xl rounded-2xl bg-gray-900 w-1/2 p-10  '>
-     <div className="mb-6  flex flex-col space-y-2 ">
-  <label className="form-label text-lg font-semibold">Email address</label>
-  <input type="string" name='email' value={data.email} onChange={inputhandle} className="form-control py-2 text-black rounded-full px-2 focus:outline-none" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <div className='min-h-screen flex items-center justify-center  bg-gradient-to-br from-gray-950 to-blue-950 px-5'>
+     <form onSubmit={handleform} className=' shadow-xl rounded-2xl w-full max-w-md backdrop-blur-lg bg-white/10 border  border-gray-900 p-10  '>
+     <div className="mb-6  flex flex-col space-y-1 ">
+  <label className=" text-lg font-semibold">Email address</label>
+  <input type="string" name='email' value={data.email} onChange={inputhandle} className=" px-4 py-3 text-black rounded-xl focus:ring-2 focus:ring-blue-400 focus:outline-none" id="exampleFormControlInput1" placeholder="name@example.com"/>
 </div>
-<div className="mb-10 flex flex-col space-y-2 ">
+<div className="mb-10 flex flex-col space-y-1 ">
   <label className="form-label text-lg font-semibold">Password</label>
-  <input type="string" className="form-control py-2 text-black rounded-full px-2 focus:outline-none" name='password' value={data.password} onChange={inputhandle} placeholder="enter password"/>
+  <input type="string" className="form-control text-black rounded-xl px-4 py-3 focus:ring-2 focus:ring-blue-400 focus:outline-none" name='password' value={data.password} onChange={inputhandle} placeholder="enter password"/>
 </div>
-<div className="mb- w-full flex container justify-center">
-  <button type='submit' className='btn btn-primary px-14 py-2 text-lg rounded-lg hover:bg-sky-900 text-black bg-sky-700 hover:text-white font-semibold '>Login</button>
+<div className=" w-full flex container justify-center">
+  <button type='submit' className='btn btn-primary px-14 py-2 w-full text-lg rounded-lg hover:bg-sky-900  bg-sky-700 hover:text-white font-semibold '>Login</button>
 </div>
-<div className='mb- w-full flex flex-col text-sm  items-center justify-center mt-2'>
+<div className=' w-full flex flex-col text-sm  items-center justify-center mt-8'>
   <p>Do'not account ?</p>
-  <NavLink to={'/register'} className='hover:underline'>Register</NavLink>
+  <NavLink to={'/register'} className='hover:underline text-blue-500 font-semibold '>Register</NavLink>
 </div>
      </form>
     </div>
